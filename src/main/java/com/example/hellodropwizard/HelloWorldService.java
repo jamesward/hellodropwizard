5376853af94cd4f00003c63d4abd0bb445cd1350
James Ward <james@jamesward.org>
2012-03-19 22:06:31 -0600

    testing asset servlet

--- src/main/java/com/example/hellodropwizard/HelloWorldService.java
+++ src/main/java/com/example/hellodropwizard/HelloWorldService.java
@@ -2,7 +2,10 @@ package com.example.hellodropwizard;
 
 import com.example.hellodropwizard.resources.HelloWorldResource;
 import com.yammer.dropwizard.Service;
+import com.yammer.dropwizard.bundles.AssetsBundle;
 import com.yammer.dropwizard.config.Environment;
+
+import java.io.File;
 
 public class HelloWorldService extends Service<HelloWorldConfiguration> {
 
@@ -12,11 +15,12 @@ public class HelloWorldService extends Service<HelloWorldConfiguration> {
 
     private HelloWorldService() {
         super("hello-world");
+        addBundle(new AssetsBundle());
     }
 
     @Override
     protected void initialize(HelloWorldConfiguration configuration,
-                              Environment environment) {
+                              Environment environment)         {
         final String template = configuration.getTemplate();
         final String defaultName = configuration.getDefaultName();
         environment.addResource(new HelloWorldResource(template, defaultName));
